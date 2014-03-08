471c41ee10f323b56a7d7084c4359178953e36a8
mastakeu <isedaisuke@gmail.com>
2014-03-08 22:36:45 +0900

    User-Agent判定、デバイス別viewを追加

--- src/main/scala/controller/ApplicationController.scala
+++ src/main/scala/controller/ApplicationController.scala
@@ -13,6 +13,26 @@ trait ApplicationController extends SkinnyController
   // with SkinnySessionFilter
   with ErrorPageFilter {
 
+  val defaultDevice = "default"
+  val smartDevice = "smartphone"
+
+  private val extension = "ssp"
+  private val mobileRegex = """.*(?i)(iphone|android|ipod).*""".r
+  protected var device = defaultDevice
+
+  def userAgent = request.getHeader("User-Agent")
+
+  before() {
+    device = userAgent match {
+      case mobileRegex(m) => smartDevice
+      case _ => defaultDevice
+    }
+  }
+
+  override def render(path: String)(implicit format: Format = Format.HTML): String = {
+    layout(device + "." + extension)
+    super.render(path + "_" + device)(format)
+  }
   // override def defaultLocale = Some(new java.util.Locale("ja"))
 
 }
